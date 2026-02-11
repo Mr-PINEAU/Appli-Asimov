0efe52f1fbbaf3f10c9b9d51166511e94620e303
Adam PINEAU <pineauadam5@gmail.com>
2026-02-11 11:46:38 +0100

    mise en place du script de démarrage dans le package.json

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+// Importer Express
+const express = require('express');
+
+// Créer l'application Express
+const app = express();
+
+// Définir le port
+const PORT = 5000;
+
+// Middleware pour parser le JSON
+app.use(express.json());
+
+// Route de test (page d'accueil)
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur le serveur Express !');
+});
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
