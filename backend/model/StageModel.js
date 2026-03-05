3929d02411ebd968c7adf7cde81b8ee31a167f45
PINEAU <pineauadam5@gmail.com>
2026-03-05 16:06:50 +0100

    mise à jour des données (fichier remplie)

--- backend/model/StageModel.js
+++ backend/model/StageModel.js
@@ -8,27 +8,25 @@ class Stage {
   static async create(data) {
 
   const data = [
-    {id: 1, eleve_id: 1, entreprise:"Thales", tuteur: "Jean Dupont", date_debut:"2025-01-06", date_fin:"2025-02-28", evaluation:"15"},
-    {id: 2, eleve_id: 2, entreprise:"Capgemini", tuteur: "Antoine Dumont", date_debut:"2025-01-13", date_fin:"2025-02-13", evaluation:"14"},
-    {id: 3, eleve_id: 3, entreprise:"Atos", tuteur: "Claire Lefevre", date_debut:"2025-02-05", date_fin:"2025-03-06", evaluation:"13"},
-    {id: 4, eleve_id: 4, entreprise:"Hardis Group", tuteur: "Nicolas Petit", date_debut:"2025-07-02", date_fin:"2025-06-01", evaluation:"16"},
-    {id: 5, eleve_id: 5, entreprise:"Wizbii", tuteur: "Thomas Martins", date_debut:"2025-04-12", date_fin:"2025-05-13", evaluation:"18"},
-    {id: 6, eleve_id: 6, entreprise:"Alma", tuteur: "Fabrice Doulot", date_debut:"2025-02-25", date_fin:"2025-03-24", evaluation:"15"},
-    {id: 7, eleve_id: 7, entreprise:"Avenue-Web", tuteur: "Lucie Girard", date_debut:"2025-11-12", date_fin:"2025-12-11", evaluation:"19"}
+    {idStage: 1, eleve_id: 1, date_debut:"2025-01-06", date_fin:"2025-02-28", evaluation:"15"},
+    {idStage: 2, eleve_id: 2, date_debut:"2025-01-13", date_fin:"2025-02-13", evaluation:"14"},
+    {idStage: 3, eleve_id: 3, date_debut:"2025-02-05", date_fin:"2025-03-06", evaluation:"13"},
+    {idStage: 4, eleve_id: 4, date_debut:"2025-07-02", date_fin:"2025-06-01", evaluation:"16"},
+    {idStage: 5, eleve_id: 5, date_debut:"2025-04-12", date_fin:"2025-05-13", evaluation:"18"},
+    {idStage: 6, eleve_id: 6, date_debut:"2025-02-25", date_fin:"2025-03-24", evaluation:"15"},
+    {idStage: 7, eleve_id: 7, date_debut:"2025-11-12", date_fin:"2025-12-11", evaluation:"19"}
   ];
 
     const sql = `
       INSERT INTO Stage
-      (id, eleve_id, entreprise, tuteur, date_debut, date_fin, evaluation)
-      VALUES (?, ?, ?, ?, ?, ?, ?)
+      (eleve_id, date_debut, date_fin, evaluation)
+      VALUES (?, ?, ?, ?, ?)
     `;
  
     //Retranscryptage des données par MYSQL2
     const [result] = await db.execute(sql, [
-      data.id,
+      data.idStage,
       data.eleve_id,
-      data.entreprise,
-      data.tuteur,
       data.date_debut,
       data.date_fin,
       data.evaluation
@@ -47,42 +45,41 @@ class Stage {
     return rows;
   }
 
-// Trouver un utilisateur par id
-  static async findById(eleve_id) {
+// Trouver un stage par id
+  static async findById(idStage) {
 
     const [rows] = await db.execute(
-      "SELECT * FROM Stage WHERE eleve_id"
-      [eleve_id]
+      "SELECT * FROM Stage WHERE idStage"
+      [idStage]
     );
 
     return rows[0];
   }
 
 //Mettre à jour un stage
-  static async update(id, data) {
+  static async update(idStage, data) {
 
     const sql = `
       UPDATE Stage
-      SET entreprise=?, tuteur=?, date_debut=?, date_fin=?, evaluation=?
-      WHERE id=? 
+      SET date_debut=?, date_fin=?, evaluation=?
+      WHERE idStage=? AND eleve_id=? 
     `;
 
     await db.execute(sql, [       
-      data.entreprise,
-      data.tuteur,
       data.date_debut,              
       data.date_fin,
       data.evaluation,
-      id
+      idStage,
+      data.eleve_id
     ]);
   }
 
 //Supprimer un stage
-  static async delete(id) {
+  static async delete(idStage) {
 
     await db.execute(
-      "DELETE FROM Stage WHERE id = ?",
-      [id]
+      "DELETE FROM Stage WHERE idStage = ?",
+      [idStage]
     );
   }
 
