3ba5015dc7e927d076f69da617d37be3b4102b97
PINEAU <pineauadam5@gmail.com>
2026-03-25 15:22:59 +0100

    ajout du fichier d'appel des requêtes

--- AsimUT/src/main/java-client/com/asimov/api/ApiClient.java
+++ AsimUT/src/main/java-client/com/asimov/api/ApiClient.java
@@ -1,8 +1,8 @@
-package com.asimov;
+package com.asimov.api;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class ApiClient {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
---
+++ AsimUT/src/main/java-client/com/asimov/model/ClasseModel.java
@@ -0,0 +1,4 @@
+package com.asimov.model;
+
+public class ClasseModel {
+}
