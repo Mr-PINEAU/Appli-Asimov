9b619f10b3b496df2c68930ee50fe8fd6d93da47
PINEAU <pineauadam5@gmail.com>
2026-03-30 15:53:42 +0200

    Ajout et remplissage du fichier

--- AsimUT/src/main/java-client/com/asimov/model/ClasseModel.java
+++ AsimUT/src/main/java-client/com/asimov/model/ClasseModel.java
@@ -1,4 +1,27 @@
 package com.asimov.model;
 
+
+
 public class ClasseModel {
+
+    private int id_classe;
+    private String niveau;
+    private String libelle;
+
+    public ClasseModel (String niveau, String libelle) {
+
+        this.niveau = niveau;
+        this.libelle = libelle;
+
+    }
+
+
+    public int getId_classe() { return id_classe; }
+    public void setId_classe(int id_classe) { this.id_classe = id_classe; }
+
+    public String getNiveau() { return niveau; }
+    public void setNiveau(String niveau) { this.niveau = niveau; }
+
+    public String getLibelle() { return libelle; }
+    public void setLibelle(String libelle) { this.libelle = libelle; }
 }
