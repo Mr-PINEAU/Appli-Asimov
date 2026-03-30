786eaad36993e751abe759208db6dd195ab8305c
PINEAU <pineauadam5@gmail.com>
2026-03-30 14:37:48 +0200

    Mise à jour de champ

--- AsimUT/src/main/java-client/com/asimov/controller/EleveController.java
+++ AsimUT/src/main/java-client/com/asimov/controller/EleveController.java
@@ -1,18 +1,20 @@
 package com.asimov.controller;
 
-import com.asimov.model.Eleve;
+import com.asimov.model.EleveModel;
 import com.asimov.service.EleveService;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class EleveController {
 
-    @FXML private TableView<Eleve> tableEleves;
-    @FXML private TableColumn<Eleve, String> colNumeroEleve;
-    @FXML private TableColumn<Eleve, String> colDateInscription;
-    @FXML private TableColumn<Eleve, String> colActif;
-    @FXML private TableColumn<Eleve, String> colAnneeScolaire;
-    @FXML private TableColumn<Eleve, Boolean> colRedoublant;
+    @FXML private TableView<EleveModel> tableEleves;
+    @FXML private TableColumn<EleveModel, String> colNumeroEleve;
+    @FXML private TableColumn<EleveModel, String> colDateInscription;
+    @FXML private TableColumn<EleveModel, String> colActif;
+    @FXML private TableColumn<EleveModel, String> colAnneeScolaire;
+    @FXML private TableColumn<EleveModel, Boolean> colRedoublant;
 
 
 
@@ -20,17 +22,18 @@ public class EleveController {
     @FXML private TextField dateInscriptionField;
     @FXML private TextField actifField;
     @FXML private TextField anneeScolaireField;
-    @FXML private TextField redoublantField;
+    @FXML private Boolean redoublantField;
 
     private final EleveService service = new EleveService();
 
+
     @FXML
-    public void initialize() {
-        colNumeroEleve.setCellValueFactory(data -> new javafx.beans.property.SimpleStringProperty(data.getValue().getNumeroEleve()));
-        colDateInscription.setCellValueFactory(data -> new javafx.beans.property.SimpleStringProperty(data.getValue().getDateInscription()));
-        colActif.setCellValueFactory(data -> new javafx.beans.property.SimpleStringProperty(data.getValue().getActif()));
-        colAnneeScolaire.setCellValueFactory(data -> new javafx.beans.property.SimpleStringProperty(data.getValue().getAnneeScolaire()));
-        colRedoublant.setCellValueFactory(data -> new javafx.beans.property.SimpleStringProperty(data.getValue().getRedoublant()));
+    private void initialize() {
+        colNumeroEleve.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getNumeroEleve()));
+        colDateInscription.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getDateInscription()));
+        colActif.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getActif()));
+        colAnneeScolaire.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getAnneeScolaire()));
+        colRedoublant.setCellValueFactory(data -> new SimpleBooleanProperty(data.getValue().getRedoublant()));
 
         // Quand on clique sur un élève → remplir les champs
         tableEleves.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
@@ -39,7 +42,7 @@ public class EleveController {
                 dateInscriptionField.setText(newVal.getDateInscription());
                 actifField.setText(newVal.getActif());
                 anneeScolaireField.setText(newVal.getAnneeScolaire());
-                redoublantField.setText(newVal.getRedoublant());
+                redoublantCheckBox.isSelected();
 
 
             }
@@ -48,17 +51,17 @@ public class EleveController {
 
     @FXML
     private void charger() {
-        tableEleves.getItems().setAll(service.getAll());
+        tableEleves.getItems().setAll((EleveModel) service.getAll());
     }
 
     @FXML
-    private void ajouter() {
-        Eleve e = new Eleve(
+    public void ajouter() {
+        EleveModel e = new EleveModel(
                 numeroEleveField.getText(),
                 dateInscriptionField.getText(),
                 actifField.getText(),
                 anneeScolaireField.getText(),
-                redoublantField.getBoolean()
+                redoublantCheckBox.isSelected()
 
 
         );
@@ -69,15 +72,19 @@ public class EleveController {
     }
 
     @FXML
-    private void modifier() {
-        Eleve selected = tableEleves.getSelectionModel().getSelectedItem();
+    private CheckBox redoublantCheckBox;
+
+    @FXML
+    public void modifier() {
+        EleveModel selected = tableEleves.getSelectionModel().getSelectedItem();
 
         if (selected != null) {
             selected.setNumeroEleve(numeroEleveField.getText());
             selected.setDateInscription(dateInscriptionField.getText());
             selected.setActif(actifField.getText());
             selected.setAnneeScolaire(anneeScolaireField.getText());
-            selected.setRedoublant(redoublantField.getText());
+            selected.setRedoublant(redoublantCheckBox.isSelected());
+
 
             service.modifier(selected);
             charger();
@@ -85,18 +92,21 @@ public class EleveController {
     }
 
     @FXML
-    private void supprimer() {
-        Eleve selected = tableEleves.getSelectionModel().getSelectedItem();
+    public void supprimer() {
+        EleveModel selected = tableEleves.getSelectionModel().getSelectedItem();
 
         if (selected != null) {
-            service.supprimer(selected.getId());
+            service.supprimer(selected.getIdEleve());
             charger();
         }
     }
 
     private void clearFields() {
-        nomField.clear();
-        prenomField.clear();
-        classeField.clear();
+        numeroEleveField.clear();
+        dateInscriptionField.clear();
+        actifField.clear();
+        anneeScolaireField.clear();
+        redoublantField.booleanValue();
+
     }
 }
