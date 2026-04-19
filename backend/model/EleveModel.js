61301fae8a61eb8575cde414d42bbaaf03ddc399
Adam PINEAU <pineauadam5@gmail.com>
2026-04-19 14:10:05 +0200

    mise à jour de données (fichier rempli)

--- backend/model/EleveModel.js
+++ backend/model/EleveModel.js
@@ -3,36 +3,27 @@ const db = require("../config/database");
 
 class Eleve {
 
-//Créer un élève
+  //Créer un élève
   static async create(data) {
 
   const data = [
-    {id:null, idUtilisateur:3, numeroEleve: 1, dateInscription: "2024-2025", statut: "actif", anneeScolaire: "2025-2026", redoublant: true},
-];
+    {idUtilisateur: 1, idClasse: 2, numeroEleve:"ELV-2024-001", dateInscription: "2024-09-02", statut: "Actif", anneeScolaire: "2024/2025", redoublant: false},
+    {idUtilisateur: 3, idClasse:3, numeroEleve:"ELV-2024-002", dateInscription: "2024-09-02", statut: "Actif",  anneeScolaire: "2024/2025", redoublant: true},
+    {idUtilisateur: 2, idClasse: 1, numeroEleve:"ELV-2024-003", dateInscription: "2024-09-02", statut: "Actif", anneeScolaire: "2024/2025", redoublant: true},
+    {idUtilisateur: 4, idClasse:4, numeroEleve:"ELV-2024-004", dateInscription: "2024-09-02", statut: "Suspendu",  anneeScolaire: "2024/2025", redoublant: false},
+    {idUtilisateur: 2, idClasse: 4, numeroEleve:"ELV-2024-005", dateInscription: "2024-09-02", statut: "Actif", anneeScolaire: "2024/2025", redoublant: false},
+    {idUtilisateur: 1, idClasse: 5, numeroEleve:"ELV-2024-006", dateInscription: "2024-09-02", statut: "Transféré", anneeScolaire: "2024/2025", redoublant: false},
 
-
-    { idEleve: 1 , idUtilisateur: 10, idClasse: 1, numeroEleve:"ELV-2024-001", dateInscription:"2024-09-02", statut:"Actif", anneeScolaire:"2024/2025", redoublant: false},
-    { idEleve: 2 , idUtilisateur: 11, idClasse: 1, numeroEleve:"ELV-2024-002", dateInscription:"2024-09-02", statut:"Actif", anneeScolaire:"2024/2025", redoublant: true},
-    { idEleve: 3 , idUtilisateur: 12, idClasse: 2, numeroEleve:"ELV-2024-003", dateInscription:"2024-09-03", statut:"Actif", anneeScolaire:"2024/2025", redoublant: false } ,
-    { idEleve: 4 , idUtilisateur: 13, idClasse: 2, numeroEleve:"ELV-2024-004", dateInscription:"2024-09-04", statut:"Suspendue", anneeScolaire:"2024/2025", redoublant: false} ,
-    { idEleve: 5 , idUtilisateur: 14, idClasse: 3, numeroEleve:"ELV-2024-005", dateInscription:"2024-09-04", statut:"Actif", anneeScolaire:"2024/2025", redoublant: true},
-    { idEleve: 6 , idUtilisateur: 15, idClasse: 3, numeroEleve:"ELV-2025-006", dateInscription:"2024-09-04", statut:"Actif", anneeScolaire:"2024/2025", redoublant: false},
-    { idEleve: 7 , idUtilisateur: 16, idClasse: 4, numeroEleve:"ELV-2024-007", dateInscription:"2024-09-05", statut:"Transféré", anneeScolaire:"2023/2024", redoublant: false},
-    { idEleve: 8 , idUtilisateur: 17, idClasse: 5,  numeroEleve:"ELV-2024-008", dateInscription:"2024-09-05", statut:"Actif", anneeScolaire:"2024/2025", redoublant: false},
-    { idEleve: 9 , idUtilisateur: 18, idClasse: 5, numeroEleve:"ELV-2024-009", dateInscription:"2024-09-06", statut:"Actif", anneeScolaire:"2024/2025", redoublant: true},
-    { idEleve: 10 , idUtilisateur: 19, idClasse: 6, numeroEleve:"ELV-2024-010", dateInscription:"2024-09-06", statut:"Exclu", anneeScolaire:"2024/2025", redoublant: false} 
-  
-  ;
+  ];
 
     const sql = `
       INSERT INTO eleves
       (idUtilisateur, idClasse, numeroEleve, dateInscription, statut, anneeScolaire, redoublant)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+      VALUES (?, ?, ?, ?, ?, ?, ?)
     `;
- 
+
     //Retranscryptage des données par MYSQL2
     const [result] = await db.execute(sql, [
-      data.idEleve,
       data.idUtilisateur,
       data.idClasse,
       data.numeroEleve,
@@ -45,7 +36,7 @@ class Eleve {
     return result.insertId;
   }
 
-// Récupérer tous les élèves
+  // Récupérer tous les élèves
   static async findAll() {
 
     const [rows] = await db.execute(
@@ -55,7 +46,7 @@ class Eleve {
     return rows;
   }
 
-// Trouver les élèves par id
+  // Trouver les élèves par id
   static async findById(idEleve) {
 
     const [rows] = await db.execute(
@@ -66,7 +57,7 @@ class Eleve {
     return rows[0];
   }
 
-//Mettre à jour un élève
+  //Mettre à jour un élève
   static async update(idEleve, data) {
 
     const sql = `
@@ -82,18 +73,18 @@ class Eleve {
       data.anneeScolaire,
       data.redoublant,
       idEleve,
-      idUtilisateur,
-      idClasse
+      data.idUtilisateur,
+      data.idClasse
     ]);
   }
 
-//Supprimer un élève
+  //Supprimer un élève
   static async delete(idEleve) {
 
     await db.execute(
       "DELETE FROM eleves WHERE idEleve = ?",
       [idEleve]
-   );
+    );
   }
 
 }
