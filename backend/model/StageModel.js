c2899ac81522655475d98567d77095b321a335d5
adam <adam@me.fr>
2026-04-20 15:11:43 +0200

    rajout d'une boucle itérative

--- backend/model/StageModel.js
+++ backend/model/StageModel.js
@@ -7,7 +7,7 @@ class Stage {
 // Ne pas oublier d'adapter les champs de la table utilisateur avec ceux de ma modélisation Merise (looping)
   static async create(data) {
 
-  const data = [
+  const seedData = [
     {idStage: 1, eleve_id: 1, date_debut:"2025-01-06", date_fin:"2025-02-28", evaluation:"15"},
     {idStage: 2, eleve_id: 2, date_debut:"2025-01-13", date_fin:"2025-02-13", evaluation:"14"},
     {idStage: 3, eleve_id: 3, date_debut:"2025-02-05", date_fin:"2025-03-06", evaluation:"13"},
@@ -24,13 +24,15 @@ class Stage {
     `;
  
     //Retranscryptage des données par MYSQL2
-    const [result] = await db.execute(sql, [
+    for (const sql of seedData) {
+      await db.execute(sql, [
       data.idStage,
       data.eleve_id,
       data.date_debut,
       data.date_fin,
       data.evaluation
     ]);
+  }
 
     return result.insertId;
   }
