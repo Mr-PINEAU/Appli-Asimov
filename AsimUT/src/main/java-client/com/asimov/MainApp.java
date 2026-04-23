8afc9ca5b0da164fd5e055516071700d2ce56d92
adam <adam@me.fr>
2026-04-23 15:37:26 +0200

    remplisage du fichier principale

--- AsimUT/src/main/java-client/com/asimov/MainApp.java
+++ AsimUT/src/main/java-client/com/asimov/MainApp.java
@@ -1,4 +1,32 @@
 package com.asimov;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // Charger la fenêtre de connexion
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Login.fxml"));
+
+        Scene scene = new Scene(loader.load(), 600, 400);
+
+        // Titre de la fenêtre
+        stage.setTitle("Application Asimov - Collège");
+
+        // Empêcher le redimensionnement
+        stage.setResizable(false);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
