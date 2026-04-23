3a67a50e7eb86725663bee159c5aa9fd5a2b876c
adam <adam@me.fr>
2026-04-23 16:33:06 +0200

    ajout des méthodes HTPP

--- AsimUT/src/main/java-client/com/asimov/api/ApiClient.java
+++ AsimUT/src/main/java-client/com/asimov/api/ApiClient.java
@@ -1,11 +1,47 @@
 package com.asimov.api;
 
-public String put(String endpoint, String json) throws Exception {
-    HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(BASE_URL + endpoint))
-            .header("Content-Type", "application/json")
-            .PUT(HttpRequest.BodyPublishers.ofString(json))
-            .build();
 
-    return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiClient {
+
+    public static final String BASE_URL = "http://localhost:3000";
+    private HttpClient client = HttpClient.newHttpClient();
+
+    public String put(String endpoint, String json) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + endpoint))
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+    }
+    public String get(String endpoint) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + endpoint))
+                .GET()
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+    }
+    public String post(String path, String endpoint) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + endpoint))
+                .GET()
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+    }
+    public String delete(String endpoint) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + endpoint))
+                .GET()
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+    }
 }
